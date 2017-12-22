ed82e9e8b4b52d55f8f7cf0dd0e1d5aea2ea5c0d
CristianAlexLopez <32960226+CristianAlexLopez@users.noreply.github.com>
2017-12-22 16:18:35 +0100

    Initial Commit

---
+++ Wootis/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Wootis
+//
+//  Created by Alex Lopez on 22/12/17.
+//  Copyright © 2017 Wootis.inc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
