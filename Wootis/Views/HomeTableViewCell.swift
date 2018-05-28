76c2e06dd95099bebb46a0d6b87c5cdd5a349708
Cristian Lopez <alex_lopez.es@icloud.com>
2018-05-28 17:08:51 +0200

    Version 2.1
    Adding:
    
    -2 Votes

--- Wootis/Views/HomeTableViewCell.swift
+++ Wootis/Views/HomeTableViewCell.swift
@@ -8,6 +8,7 @@
 
 import UIKit
 import FirebaseDatabase
+import FirebaseAuth
 
 class HomeTableViewCell: UITableViewCell {
     @IBOutlet weak var profileImageView: UIImageView!
@@ -17,6 +18,7 @@ class HomeTableViewCell: UITableViewCell {
     @IBOutlet weak var postImageTwo: UIImageView!
     @IBOutlet weak var imageTwoTitle: UILabel!
     @IBOutlet weak var VoteOneView: UIImageView!
+    @IBOutlet weak var VoteTwoView: UIImageView!
     @IBOutlet weak var CommentsView: UIImageView!
     @IBOutlet weak var shareView: UIImageView!
     @IBOutlet weak var votesAccounts: UIButton!
@@ -51,6 +53,20 @@ class HomeTableViewCell: UITableViewCell {
         } else {
             postImageTwo.image = UIImage(named: "post-test")
         }
+        
+        if let currentUser = Auth.auth().currentUser {
+            Api.User.REF_USERS.child((currentUser.uid)).child("votes").child((post?.postId)!).observeSingleEvent(of: .value, with: { snapshot in
+                if let _ = snapshot.value as? NSNull {
+                    self.VoteOneView.image = UIImage(named: "vote")
+                } else {
+                    if snapshot.value == "first"{
+                        self.VoteOneView.image = UIImage(named: "voted")
+                    } else {
+                        self.VoteTwoView.image = UIImage(named: "voted")
+                    }
+                }
+            })
+        }
     }
     
     func userInfo() {
@@ -68,11 +84,53 @@ class HomeTableViewCell: UITableViewCell {
         let TapGesture = UITapGestureRecognizer(target: self, action: #selector(self.commentsView_Touch))
         CommentsView.addGestureRecognizer(TapGesture)
         CommentsView.isUserInteractionEnabled = true
+        
+        let TapGestureVoteOne = UITapGestureRecognizer(target: self, action: #selector(self.VoteOneView_Touch))
+        VoteOneView.addGestureRecognizer(TapGestureVoteOne)
+        VoteOneView.isUserInteractionEnabled = true
+        
+        let TapGestureVoteTwo = UITapGestureRecognizer(target: self, action: #selector(self.VoteTwoView_Touch))
+        VoteTwoView.addGestureRecognizer(TapGestureVoteTwo)
+        VoteTwoView.isUserInteractionEnabled = true
     }
     
     @objc func commentsView_Touch() {
         if let id = post?.postId {
             homeViewController?.performSegue(withIdentifier: "CommentSegue", sender: id)
+        }
+    }
+    
+    @objc func VoteOneView_Touch() {
+        if let currentUser = Auth.auth().currentUser {
+            Api.User.REF_USERS.child((currentUser.uid)).child("votes").child((post?.postId)!).observeSingleEvent(of: .value, with: { snapshot in
+                if let _ = snapshot.value as? NSNull {
+                    Api.User.REF_USERS.child(currentUser.uid).child("votes").child(self.post!.postId!).setValue("first")
+                    self.VoteOneView.image = UIImage(named: "voted")
+                } else {
+                    Api.User.REF_USERS.child(currentUser.uid).child("votes").child(self.post!.postId!).removeValue()
+                    if self.VoteTwoView.image == UIImage(named: "voted"){
+                        self.VoteTwoView.image = UIImage(named: "vote")
+                    }
+                    self.VoteOneView.image = UIImage(named: "vote")
+                }
+            })
+        }
+    }
+    
+    @objc func VoteTwoView_Touch() {
+        if let currentUser = Auth.auth().currentUser {
+            Api.User.REF_USERS.child((currentUser.uid)).child("votes").child((post?.postId)!).observeSingleEvent(of: .value, with: { snapshot in
+                if let _ = snapshot.value as? NSNull {
+                    Api.User.REF_USERS.child(currentUser.uid).child("votes").child(self.post!.postId!).setValue("second")
+                    self.VoteTwoView.image = UIImage(named: "voted")
+                } else {
+                    Api.User.REF_USERS.child(currentUser.uid).child("votes").child(self.post!.postId!).removeValue()
+                    if self.VoteOneView.image == UIImage(named: "voted"){
+                        self.VoteOneView.image = UIImage(named: "vote")
+                    }
+                    self.VoteTwoView.image = UIImage(named: "vote")
+                }
+            })
         }
     }
     
