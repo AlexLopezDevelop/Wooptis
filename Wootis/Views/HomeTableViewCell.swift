d18f87fad7e5695d7185c92726b44a9321594c69
Cristian Lopez <32960226+CristianLopezDev@users.noreply.github.com>
2018-05-28 18:31:54 +0200

    Version 2.3
    Adding:
    
    - View home edit

--- Wootis/Views/HomeTableViewCell.swift
+++ Wootis/Views/HomeTableViewCell.swift
@@ -25,6 +25,7 @@ class HomeTableViewCell: UITableViewCell {
     @IBOutlet weak var commentLabel: UILabel!
     
     var homeViewController: HomeViewController?
+    var postRef: DatabaseReference!
     
     var post: Post? {
         didSet {
@@ -54,7 +55,7 @@ class HomeTableViewCell: UITableViewCell {
             postImageTwo.image = UIImage(named: "post-test")
         }
         
-        if let currentUser = Auth.auth().currentUser {
+        /*if let currentUser = Auth.auth().currentUser {
             Api.User.REF_USERS.child((currentUser.uid)).child("votes").child((post?.postId)!).observeSingleEvent(of: .value, with: { snapshot in
                 if let _ = snapshot.value as? NSNull {
                     self.VoteOneView.image = UIImage(named: "vote")
@@ -66,7 +67,7 @@ class HomeTableViewCell: UITableViewCell {
                     }
                 }
             })
-        }
+        }*/
     }
     
     func userInfo() {
@@ -101,7 +102,9 @@ class HomeTableViewCell: UITableViewCell {
     }
     
     @objc func VoteOneView_Touch() {
-        if let currentUser = Auth.auth().currentUser {
+        postRef = Api.Post.REF_POSTS.child(post!.postId!)
+        incrementVotes(forRef: postRef)
+        /*if let currentUser = Auth.auth().currentUser {
             Api.User.REF_USERS.child((currentUser.uid)).child("votes").child((post?.postId)!).observeSingleEvent(of: .value, with: { snapshot in
                 if let _ = snapshot.value as? NSNull {
                     Api.User.REF_USERS.child(currentUser.uid).child("votes").child(self.post!.postId!).setValue("first")
@@ -114,7 +117,7 @@ class HomeTableViewCell: UITableViewCell {
                     self.VoteOneView.image = UIImage(named: "vote")
                 }
             })
-        }
+        }*/
     }
     
     @objc func VoteTwoView_Touch() {
@@ -131,6 +134,39 @@ class HomeTableViewCell: UITableViewCell {
                     self.VoteTwoView.image = UIImage(named: "vote")
                 }
             })
+        }
+    }
+    
+    func incrementVotes(forRef ref: DatabaseReference) { //Firebase Transactions
+        ref.runTransactionBlock({ (currentData: MutableData) -> TransactionResult in
+            if var post = currentData.value as? [String : AnyObject], let uid = Auth.auth().currentUser?.uid {
+                print("value 1: \(currentData.value)")
+                var votes: Dictionary<String, Bool>
+                votes = post["votes"] as? [String : Bool] ?? [:]
+                var votesCount = post["votesCount"] as? Int ?? 0
+                if let _ = votes[uid] {
+                    // Unstar the post and remove self from stars
+                    votesCount -= 1
+                    votes.removeValue(forKey: uid)
+                } else {
+                    // Star the post and add self to stars
+                    votesCount += 1
+                    votes[uid] = true
+                }
+                post["votesCount"] = votesCount as AnyObject?
+                post["votes"] = votes as AnyObject?
+                
+                // Set value and report transaction success
+                currentData.value = post
+                
+                return TransactionResult.success(withValue: currentData)
+            }
+            return TransactionResult.success(withValue: currentData)
+        }) { (error, committed, snapshot) in
+            if let error = error {
+                print(error.localizedDescription)
+            }
+            print("value 2: \(snapshot?.value)")
         }
     }
     
