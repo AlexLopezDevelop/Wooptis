cd18e0131cce6ed37fc6f4e3fb852186c689d9a9
Cristian Lopez <32960226+CristianLopezDev@users.noreply.github.com>
2018-05-29 21:19:54 +0200

    Version 3.5
    Added:
    
    - CellView (Post) Image fixed

---
+++ Wootis/Styles/UIBotones.swift
@@ -0,0 +1,37 @@
+//
+//  UIBotones.swift
+//  Wootis
+//
+//  Created by qwerty on 29/5/18.
+//  Copyright © 2018 Wootis.inc. All rights reserved.
+//
+
+import UIKit
+
+@IBDesignable
+class UIBotones: UIButton {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+    
+    @IBInspectable
+    public var cornerRadius: CGFloat = 2.0 {
+        didSet {
+            self.layer.cornerRadius = self.cornerRadius
+        }
+    }
+    
+    
+    override var backgroundColor: UIColor? {
+        didSet {
+           self.layer.backgroundColor = UIColor.blue.cgColor
+        }
+    }
+    
+}
